1a3cca2fce4b8a62e137ce02ec749eafd63651ee
Hekar Khani <Hekar.Khani@D2L.com>
2019-08-09 11:35:08 -0400

    Export XRay driver from library

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const DynamoDBDocumentClient = require('./lib/dynamodb-document-client');
 const S3 = require('./lib/s3');
 const SQS = require('./lib/sqs');
+const XRay = require('./lib/xray');
 
 module.exports = {
   DynamoDBDocumentClient,
   S3,
-  SQS
+  SQS,
+  XRay
 };
